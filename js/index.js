2c737b6a8985c685ba5e69c71f574ebedfb54057
@githubAdmin123 <certgendict@gmail.com>
2023-04-18 13:52:27 +0800

    push

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// Import the Supabase client
+import { createClient } from '@supabase/supabase-js';
+
+// Initialize the Supabase client with your Supabase project URL and API key
+const supabase = createClient('https://tcxiwsgjupnlfzoncypc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjeGl3c2dqdXBubGZ6b25jeXBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE3OTY4MDYsImV4cCI6MTk5NzM3MjgwNn0.hHIDaZcQenuWJDLouK0dZPta0A1ZftnoR19ddNDJPmE');
